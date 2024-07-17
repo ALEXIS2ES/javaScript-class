828296933712c7e2feefdca7afa9729612ead74d
alexis2es <vccamerccoa@unac.edu.co>
2024-07-17 08:21:12 -0500

    tipos de datos

---
+++ scripts.js
@@ -0,0 +1,52 @@
+// alert("Hola Mundo Nuevo De Js")
+/*
+comentarios de bloque
+haciendo novedades
+las cuales no se
+*/
+
+/**
+ * este archivo representa el buscador de usuario
+ * es ma elegante
+ * es un estandar
+ * nada mas
+ */
+
+
+//var hola = "Saludar"
+//console.log(hola)
+//console.log("Adios")  ctrl + shit + 7
+
+/**
+ * TIPOS DE DATOS
+ */
+
+//Number (para todos los tipos de numero, enteros, decimales)
+console.log(typeof 50.6)
+
+//string
+console.log(typeof "Alexis CCamerccoa")
+
+//boolean = true, false
+console.log(typeof false)
+
+/**
+ * TIPOS DE DATOS = Colecion de datos
+ * colecciones de datos(informacion)
+ * que tienen un identificador y un valor
+ */
+
+//ARRAY (arreglo)
+
+console.log(typeof["jose", "josé", "juan", "jean", "oto"])
+
+//objetos  de js
+
+console.log(typeof{
+    nombre: "Alexis", 
+    apellido: "CCamerccoa"
+})
+
+//NULO Null
+
+console.log(typeof null)
