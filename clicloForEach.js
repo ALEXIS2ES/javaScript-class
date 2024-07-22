4b8670f12b15fafc2c603ce5d9beaf67adb8d047
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 13:51:40 -0500

    .map

--- clicloForEach.js
+++ clicloForEach.js
@@ -7,3 +7,13 @@ let dato = amigos.forEach(amigo => console.log(`Hola ${amigo}`))
 
 console.log(amigos)
 //foreach hace algo con los elementos
+
+/**
+ * .map
+ */
+// tiene la misma funcionalidad de for each pero .map añade un nuevo array con cada clico de un nuevo dato 
+
+let datos = amigos.map(amigo => `Hola ${amigo}`)
+
+console.log(amigos)
+console.log(datos)
