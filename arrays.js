35fc86ab1fb9d0bc212bb69d71689f0caece2e6a
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 13:35:53 -0500

    dividir a dor arrays

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@
 let amigo = []
 
 let amigos = ["alexis", "victor", "ccamerccoa", "mamani",]
-
+console.log(amigos)
 /**
  * //agregar elementos a ami array
 let dato = amigos.push("gaston")
@@ -13,10 +13,16 @@ console.log(dato)
 console.log(amigos)
  */
 
-console.log(amigos)
-//quitar elementos a ami array
+/**
+ * //quitar elementos a ami array
 let dato = amigos.pop()
 
+console.log(dato)
+console.log(amigos)
+ */
+
+//partir el arrays en 2 arrays
+let dato = amigos.slice(0,2)
 
 console.log(dato)
 console.log(amigos)
