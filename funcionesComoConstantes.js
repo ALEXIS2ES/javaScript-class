76a8de4856e2ce234f84bb4daf603ae063fa202c
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 12:37:26 -0500

    funciones como constantes

---
+++ funcionesComoConstantes.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * funciones como constantes
+ * 
+ * las funciones siempre debe estar en una constante
+ */
+const saludar = function(nombre){
+    console.log(`hola ${nombre}`)
+
+}
+console.log(saludar("juan"))
