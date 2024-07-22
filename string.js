928b5a0099b1552ca2e9bf9e336208a967650597
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 14:37:14 -0500

    string

---
+++ string.js
@@ -0,0 +1,22 @@
+//slice
+let texto = "victor alexis ccamerccoa mamani"
+
+let dato = texto.slice(3, 10)
+
+console.log(dato)
+
+//split este es una coleccionde datos y en un array
+let datos = texto.split("")
+
+console.log(datos)
+
+//search dentro del parametro necesita una expresion regular
+let dat = texto.search()
+
+console.log(dat)
+
+//como convertir a mayusculas y minusculas
+
+let da = texto.toLocaleUpperCase()
+
+console.log(da)
