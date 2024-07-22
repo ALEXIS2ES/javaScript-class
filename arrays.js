1fe468094eb3a5f6faed0e9e91d848baec24252b
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 13:28:44 -0500

    quitar datos

--- arrays.js
+++ arrays.js
@@ -6,8 +6,17 @@ let amigo = []
 
 let amigos = ["alexis", "victor", "ccamerccoa", "mamani",]
 
-//agregar elementos a ami array
+/**
+ * //agregar elementos a ami array
 let dato = amigos.push("gaston")
 console.log(dato)
 console.log(amigos)
+ */
 
+console.log(amigos)
+//quitar elementos a ami array
+let dato = amigos.pop()
+
+
+console.log(dato)
+console.log(amigos)
