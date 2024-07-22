93bf64abfe5011a8b54b307e200c403e0c0a6135
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 14:00:35 -0500

    filter

--- filter.js
+++ filter.js
@@ -4,12 +4,5 @@
 
 let numeros = [10,436,45,74,33,9,2,54]
 
-let nuevoArray = []
-
-numeros.map(num => {
-    if(num > 20){
-        nuevoArray.push(num)
-    }
-})
-console.log(nuevoArray)
-console.log(numeros)
+let dato = numeros.filter(num => num < 20)
+console.log(dato)
