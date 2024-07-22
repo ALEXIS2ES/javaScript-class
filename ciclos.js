3db5e50c626ff7805765e5e4182205407ef02be1
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 11:34:41 -0500

    cliclos

--- ciclos.js
+++ ciclos.js
@@ -3,3 +3,27 @@
  * bluques o loops
  */
 
+//for
+/**
+ * for (valor de inicializacion; condicion;  valor o incremento){
+    imprima el valor del ciclo atravez de la consola
+}
+ */
+
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
+
+/**
+ * ejercicio de for
+ * 
+ */
+
+// dado su contenido un texto deseamos imprimir su contenido letra por letra
+//"alexis"
+
+let texto = "alexisCCamerccoa"
+
+for(let i = 0;i < texto.length; i++){
+    console.log(texto[i])
+}
