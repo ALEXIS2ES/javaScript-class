e7572dc118142d214a5db96461b7d20f632d1c2d
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 11:46:23 -0500

    ciclo while

---
+++ ciclo while.js
@@ -0,0 +1,16 @@
+/**
+ * ciclos while
+ * 
+ * mientras
+ * 
+ * while (accion a realizar mientras){
+    
+}
+ */
+
+let juguetes = 10
+
+while(juguetes > 0){
+    juguetes--
+    console.log(`regalamos un juguetes. Nos quedan ${juguetes-1}`)
+}
