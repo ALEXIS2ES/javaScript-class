8cdbd55d78830cc987e6e74a1920187828198091
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 13:25:10 -0500

    arrays anadiendo al array

---
+++ arrays.js
@@ -0,0 +1,13 @@
+/**
+ * arrays
+ */
+
+let amigo = []
+
+let amigos = ["alexis", "victor", "ccamerccoa", "mamani",]
+
+//agregar elementos a ami array
+let dato = amigos.push("gaston")
+console.log(dato)
+console.log(amigos)
+
