450a909894f8fbb82674f0effe74dc8309a90861
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 13:47:20 -0500

    foreach

---
+++ clicloForEach.js
@@ -0,0 +1,9 @@
+
+let amigos = ["alexis", "victor", "ccamerccoa", "mamani",]
+
+//foreach necesita como parametro una funcion
+
+let dato = amigos.forEach(amigo => console.log(`Hola ${amigo}`))
+
+console.log(amigos)
+//foreach hace algo con los elementos
