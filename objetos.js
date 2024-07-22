708d83b767a935e8488fdfaec38cdad4d1ef688e
alexis2es <vccamerccoa@unac.edu.co>
2024-07-22 17:08:21 -0500

    math y date

---
+++ objetos.js
@@ -0,0 +1,30 @@
+/**
+ * objetos
+ */
+
+let alumnos = {
+    nombre: "alexis",
+    edad : 21,
+    suscriptor: false,
+    ciudad: "lima"
+}
+// values para valores y keys para los campos  
+let valores = Object.values(alumnos)
+
+console.log(valores)
+
+/**
+ * math y date
+ * datos numericos y datos fecha
+ */
+
+//math
+const valo = Math.random()*10
+
+console.log(valo)
+
+//math 
+const valor = new Date
+
+console.log(valor.getUTCDate())
+
